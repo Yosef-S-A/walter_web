6f2d5f95d609f608772269721216bd95ad96717c
Yosef-S-A <yosefsamuel22@gmail.com>
2022-07-27 12:33:23 +0300

    first setup

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <editor v-model="content" />
+
+    <div class="content">
+      <h3>Content</h3>
+      <pre><code>{{ content }}</code></pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import Editor from "../components/EditorComponent.vue";
+
+export default {
+  components: {
+    Editor,
+  },
+  data() {
+    return {
+      content: "",
+    };
+  },
+};
+</script>
